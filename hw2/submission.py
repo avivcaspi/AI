72031d607ad803f311de83dca73a26bbc80c44a3
avivcaspi <avivcaspi@gmail.com>
2019-12-19 19:36:19 +0200

    question 5-6

--- hw2/submission.py
+++ hw2/submission.py
@@ -17,13 +17,16 @@ def heuristic(state: GameState, player_index: int) -> float:
         return state.snakes[player_index].length
     closest_apple = _find_nearest_apple_dist(state, player_index)
     if closest_apple > 0:
-        return state.snakes[player_index].length + 0.8 / closest_apple + 0.2*_find_cluster_around_apples(state, player_index, 3)
+        return state.snakes[player_index].length + 0.8 / closest_apple
     return state.snakes[player_index].length
 
 
 def _find_nearest_apple_dist(state: GameState, player_index: int) -> float:
     head = state.snakes[player_index].head
-    return np.min([np.linalg.norm(np.array(head) - np.array(apple)) for apple in state.fruits_locations])
+    dists = [np.linalg.norm(np.array(head) - np.array(apple)) for apple in state.fruits_locations]
+    if len(dists) > 0:
+        return np.min(dists)
+    return 1
 
 
 def _find_nearest_apple_num(state: GameState, pos: tuple, r: int) -> float:
@@ -77,8 +80,6 @@ class MinimaxAgent(Player):
     def get_action(self, state: GameState) -> GameAction:
         turn_state = self.TurnBasedGameState(state, agent_action=None)
         _, action = self.rb_minimax(turn_state, self.player_index, 2)
-        if action is None:
-            return GameAction.STRAIGHT
         return action
 
     def rb_minimax(self, state: TurnBasedGameState, player_index: int, depth: int):
@@ -112,12 +113,13 @@ class MinimaxAgent(Player):
 
 class AlphaBetaAgent(MinimaxAgent):
     def get_action(self, state: GameState) -> GameAction:
-        pass
+        turn_state = self.TurnBasedGameState(state, agent_action=None)
+        _, action = self.alphabeta(turn_state, self.player_index, 3, -np.inf, np.inf)
+        return action
 
-    def alphabeta(self, state: TurnBasedGameState, player_index: int, depth: int):
+    def alphabeta(self, state: MinimaxAgent.TurnBasedGameState, player_index: int, depth: int, alpha: float, beta: float):
         if state.turn == MinimaxAgent.Turn.AGENT_TURN:
-            if state.game_state.turn_number == state.game_state.game_duration_in_turns or not \
-            state.game_state.snakes[player_index].alive:
+            if state.game_state.turn_number == state.game_state.game_duration_in_turns or not state.game_state.snakes[player_index].alive:
                 return state.game_state.snakes[player_index].length, state.agent_action
             if depth == 0:
                 return heuristic(state.game_state, player_index), state.agent_action
@@ -125,23 +127,30 @@ class AlphaBetaAgent(MinimaxAgent):
             max_value = -np.inf
             for action in state.game_state.get_possible_actions(player_index=player_index):
                 turn_state = self.TurnBasedGameState(state.game_state, action)
-                next_state_value, _ = self.rb_minimax(turn_state, player_index, depth)
+                next_state_value, _ = self.alphabeta(turn_state, player_index, depth, alpha, beta)
                 if next_state_value > max_value:
                     best_action = action
                     max_value = next_state_value
+                alpha = max(max_value, alpha)
+                if max_value >= beta:
+                    return np.inf, best_action
             return max_value, best_action
         else:
             best_action = None
             min_value = np.inf
             for opponents_actions in state.game_state.get_possible_actions_dicts_given_action(state.agent_action,
-                                                                                              player_index=self.player_index):
+                                                                                   player_index=self.player_index):
                 next_state = get_next_state(state.game_state, opponents_actions)
                 turn_state = self.TurnBasedGameState(next_state, None)
-                next_state_value, action = self.rb_minimax(turn_state, player_index, depth - 1)
+                next_state_value, action = self.alphabeta(turn_state, player_index, depth - 1, alpha, beta)
                 if next_state_value < min_value:
                     best_action = action
                     min_value = next_state_value
+                beta = min(min_value, beta)
+                if min_value <= alpha:
+                    return -np.inf, best_action
             return min_value, best_action
+
 
 def SAHC_sideways():
     """
@@ -156,7 +165,23 @@ def SAHC_sideways():
     3) print the best moves vector you found.
     :return:
     """
-    pass
+    n = 50
+    actions = [GameAction.STRAIGHT for _ in range(n)]
+    for j in range(3):
+        for i in range(n):
+            best_action = []
+            best_value = -np.inf
+            for action in list(GameAction):
+                actions[i] = action
+                fitness = get_fitness(tuple(actions))
+                if fitness > best_value:
+                    best_value = fitness
+                    best_action = [action]
+                elif fitness == best_value:
+                    best_action.append(action)
+            actions[i] = np.random.choice(best_action)
+
+    print(actions)
 
 
 def local_search():
@@ -172,7 +197,38 @@ def local_search():
     3) print the best moves vector you found.
     :return:
     """
-    pass
+    l = 50
+    n = 10
+    pc = 0.9
+    pm = 0.2
+    num_of_generations = 30
+    generation = np.random.choice(list(GameAction), (n, l))
+    for _ in range(num_of_generations):
+        fitness_list = [get_fitness(tuple(actions)) for actions in generation]
+        total_fitness = np.sum(fitness_list)
+        fitness_list = fitness_list / total_fitness
+        next_gen = []
+        for _ in range(n//2):
+            parents = np.random.choice(n, 2, p=fitness_list)
+            par1 = generation[parents[0]]
+            par2 = generation[parents[1]]
+            kid1 = par1
+            kid2 = par2
+            if np.random.uniform() > pc:
+                crossover_index = np.random.choice(l)
+                kid1[:crossover_index] = par1[:crossover_index]
+                kid1[crossover_index:] = par2[crossover_index:]
+                kid2[:crossover_index] = par2[:crossover_index]
+                kid2[crossover_index:] = par1[crossover_index:]
+            for i in range(l):
+                if np.random.uniform() < pm:
+                    kid1[i] = np.random.choice(list(GameAction))
+                if np.random.uniform() < pm:
+                    kid2[i] = np.random.choice(list(GameAction))
+            next_gen.extend([kid1, kid2])
+        generation = next_gen
+    fitness_list = [get_fitness(tuple(actions)) for actions in generation]
+    print(generation[np.argmax(np.array(fitness_list))])
 
 
 class TournamentAgent(Player):
@@ -182,6 +238,6 @@ class TournamentAgent(Player):
 
 
 if __name__ == '__main__':
-    SAHC_sideways()
+    #SAHC_sideways()
     local_search()
 
